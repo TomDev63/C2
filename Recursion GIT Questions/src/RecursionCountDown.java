ae820d3c318136730821022b18741e11c1d22062
T D <tom.devitt2@mail.dcu.ie>
2021-01-29 16:34:39 +0000

    Test first commit
    Change-Id: Id509924aa603929e6a58b0c33e0cd717e0aa1cfd

---
+++ Recursion GIT Questions/src/RecursionCountDown.java
@@ -0,0 +1,27 @@
+// LInk to Alex Lee YouTube recursion  https://www.youtube.com/watch?v=sE0sH8vSmE4
+// Eclipse GIT for Beginners https://www.youtube.com/results?search_query=git+eclipse+tutorial+for+beginners+v
+
+public class RecursionCountDown
+	{
+
+		public static void main(String[] args)
+			{
+				countDown(10);
+			}
+
+		public static void countDown(int n) 
+			{
+				if (n==0) 
+					{
+						System.out.println("Done!");
+					}
+				else
+					{
+						System.out.println("This cycle : " +n);
+						n--;
+						countDown(n);
+					}
+			}
+		
+		// Need a  base case
+	}
